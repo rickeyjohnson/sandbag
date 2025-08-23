089ebd725a848664daa694c1161462975157f96b
rickeyjohnson <rdjwinter2004@gmail.com>
2025-08-23 18:31:13 -0400

    Initial Xcode project setup

---
+++ Sandbag/Sandbag/SandbagApp.swift
@@ -0,0 +1,17 @@
+//
+//  sandbagApp.swift
+//  sandbag
+//
+//  Created by Rickey Johnson on 8/23/25.
+//
+
+import SwiftUI
+
+@main
+struct sandbagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
